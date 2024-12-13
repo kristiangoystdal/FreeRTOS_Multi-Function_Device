39126e47ba84ff45c56b03656886a68b13128624
Joao-Pedro-Cabral <miranda.jp@usp.br>
2024-12-13 17:08:16 +0000

    Adding professor's code for RTC

--- main.cpp
+++ main.cpp
@@ -1,29 +1,36 @@
-#include "C12832.h"
-#include "LM75B.h"
 #include "mbed.h"
+#include "RTC.h"
 
-C12832 lcd(p5, p7, p6, p8, p11);
+DigitalOut led(LED1);
 
-LM75B sensor(p28, p27);
-static Serial pc(USBTX, USBRX);
+void ledFunction( void )
+{
+    led = 1;
+    RTC::detach(RTC::Second);
+}
 
-int main() {
-  pc.baud(115200);
+void displayFunction( void )
+{
+    time_t seconds = time(NULL);
+    printf("%s", ctime(&seconds));
+}
 
-  while (1) {
-    // Try to open the LM75B
-    if (sensor.open()) {
-      printf("Device detected!\n");
+void alarmFunction( void )
+{
+    error("Not most useful alarm function");
+}
 
-      while (1) {
-        lcd.cls();
-        lcd.locate(0, 3);
-        lcd.printf("Temp = %.3f\n", sensor.temp());
-        wait(1.0);
-      }
+int main()
+{
+    set_time(1256729737); // Set time to Wed, 28 Oct 2009 11:35:37
 
-    } else {
-      error("Device not detected!\n");
-    }
-  }
+    tm t = RTC::getDefaultTM();
+    t.tm_sec = 5;
+    t.tm_min = 36;
+
+    RTC::alarm(&alarmFunction, t);
+    RTC::attach(&displayFunction, RTC::Second);
+    RTC::attach(&ledFunction, RTC::Minute);
+
+    while(1);
 }
