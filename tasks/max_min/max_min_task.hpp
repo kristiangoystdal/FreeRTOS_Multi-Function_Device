7485f3214697954f566c7e242bb51c0730ad3210
Joao-Pedro-Cabral <miranda.jp@usp.br>
2024-12-17 15:48:15 +0000

    Fixing messages sent by temperature task

---
+++ tasks/max_min/max_min_task.hpp
@@ -0,0 +1,16 @@
+
+#ifndef __MAX_MIN_TASK_H
+#define __MAX_MIN_TASK_H
+
+#include "time.h"
+
+namespace max_min_task {
+
+  typedef struct {
+    float xTemp;
+    time_t xTime;
+  } MaxMinMessage_t;
+  
+}
+
+#endif
--- tasks/temperature/temperature_task.cpp
+++ tasks/temperature/temperature_task.cpp
@@ -2,7 +2,8 @@
 #include "global_structures.h"
 #include "configuration.hpp"
 #include "LM75B.h"
-#include "time.h"
+#include <time.h>
+#include "alarm_task.hpp"
 
 namespace temperature_task {
 
@@ -17,16 +18,25 @@ namespace temperature_task {
       float xTemp = sensor.temp();
       time_t xMeasureTime = time(NULL);
 
+      max_min_task::MaxMinMessage_t xMaxMinMessage;
+      xMaxMinMessage.xTime = xMeasureTime;
+      xMaxMinMessage.xTemp = xTemp;
       BaseType_t xStatus = xQueueSend(xQueueMaxMin, &xTemp, 0);
       if(xStatus == errQUEUE_FULL){
         printf("ERROR: Queue full: Temperature -> Max/Min");
       }
 
+      lcd_task::LCDMessage_t xLCDMessage;
+      xLCDMessage.xDataType = lcd_task::LCDDataType::Temperature;
+      xLCDMessage.xLCDData = xTemp;
       BaseType_t xStatus = xQueueSend(xQueueLCD, &xTemp, 0);
       if(xStatus == errQUEUE_FULL){
         printf("ERROR: Queue full: Temperature -> LCD");
       }
 
+      alarm_task::AlarmMessage_t xAlarmMessage;
+      xAlarmMessage.xTime = xMeasureTime;
+      xAlarmMessage.xTemp = xTemp;
       BaseType_t xStatus = xQueueSend(xQueueAlarm, &xTemp, 0);
       if(xStatus == errQUEUE_FULL){
         printf("ERROR: Queue full: Temperature -> Alarm");
