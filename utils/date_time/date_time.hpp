7b7ac77f5f25b1787fb156ac1f1f565099153a2e
Kristian Goystdal <krisgoy100@gmail.com>
2025-01-11 15:59:45 +0000

    Rename set_date to set_dateTime for clarity in date_time.hpp

--- utils/date_time/date_time.hpp
+++ utils/date_time/date_time.hpp
@@ -12,8 +12,7 @@ void get_date(char *s);
 void get_clock(char *s);
 
 void set_time_t(time_t t);
-void set_date(char *s);
-void set_clock(char *s);
+void set_dateTime(char *s);
 
 time_t clock_to_time(tm *t);
 tm *time_to_clock(time_t t);
