0f6cae30f88932e828b507ebb5bf27b54ae3236b
Kristian Gøystdal <krisgoy100@gmail.com>
2025-01-12 17:33:54 +0000

    fix hitbit

--- tasks/hit_bit/hit_bit_task.hpp
+++ tasks/hit_bit/hit_bit_task.hpp
@@ -4,15 +4,15 @@
 
 namespace hit_bit_task {
 
-  #define PLAY_MODE_UPDATE_TIME 1000
-  #define PLAY_MODE_DECREMENT_TIME 50
-  #define PLAY_MODE_MINIMUM_TIME 300
-  #define WIN_MODE_UPDATE_TIME 1000
-  #define JOYSTICK_DELAY_TIME 20
+#define PLAY_MODE_UPDATE_TIME 1000
+#define PLAY_MODE_DECREMENT_TIME 50
+#define PLAY_MODE_MINIMUM_TIME 300
+#define WIN_MODE_UPDATE_TIME 1000
+#define JOYSTICK_DELAY_TIME 5
 
-  bool xGetHitBitEnabled();
-  void vSetHitBitEnabled(bool enabled);
-  void vHitBitTask(void* pvParameters);
-}
+bool xGetHitBitEnabled();
+void vSetHitBitEnabled(bool enabled);
+void vHitBitTask(void *pvParameters);
+} // namespace hit_bit_task
 
 #endif // __HIT_BIT_TASK_H
